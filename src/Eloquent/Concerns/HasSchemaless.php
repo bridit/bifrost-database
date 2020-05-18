947f5834ffbaa06f4d333edea572a5039aca0e96
Gustavo Siqueira <gus@brid-it.com>
2020-05-18 16:37:56 -0300

    fix: improving separation of concerns

---
+++ src/Eloquent/Concerns/HasSchemaless.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Bifrost\Database\Eloquent\Concerns;
+
+use Throwable;
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\SchemalessAttributes\SchemalessAttributes;
+
+trait HasSchemaless
+{
+
+  /**
+   * Model schemaless attributes.
+   *
+   * @var array
+   */
+  protected array $schemalessAttributes = [];
+
+  protected function hasSchemalessBehaviour(string $key): bool
+  {
+    return in_array($key, $this->schemalessAttributes);
+  }
+
+  /**
+   * @param string $key
+   * @return SchemalessAttributes
+   */
+  protected function getSchemalessAttribute(string $key): SchemalessAttributes
+  {
+    return SchemalessAttributes::createForModel($this, $key);
+  }
+
+  /**
+   * Scope a query to apply json conditions.
+   *
+   * @param Builder $builder
+   * @param mixed ...$arguments
+   * @return Builder
+   * @throws Throwable
+   */
+  public function scopeWithJson($builder, ...$arguments): Builder
+  {
+    $schemalessAttributes = [];
+    $attributeName = Arr::first($this->schemalessAttributes) ?? 'json_data';
+
+    if (count($arguments) === 0) {
+      return $builder->whereNotNull($attributeName);
+    }
+
+    if (count($arguments) === 1) {
+      [$schemalessAttributes] = $arguments;
+      throw_if(!is_array($schemalessAttributes), new UnexpectedValueException('Array of conditions expected.'));
+    }
+
+    if (count($arguments) === 2) {
+      [$name, $value] = $arguments;
+      $schemalessAttributes = [$name => $value];
+    }
+
+    if (count($arguments) >= 3) {
+      [$attributeName, $name, $value] = $arguments;
+      $schemalessAttributes = [$name => $value];
+    }
+
+    foreach ($schemalessAttributes as $name => $value) {
+      $builder->where(str_replace('.', '->', "{$attributeName}->{$name}"), $value);
+    }
+
+    return $builder;
+  }
+
+}
--- src/Eloquent/Model.php
+++ src/Eloquent/Model.php
@@ -2,21 +2,17 @@
 
 namespace Bifrost\Database\Eloquent;
 
-use Throwable;
-use Illuminate\Support\Arr;
-use UnexpectedValueException;
-use Illuminate\Database\Eloquent\Builder;
 use Bifrost\Database\Eloquent\Concerns\HasHashes;
 use Bifrost\Database\Eloquent\Concerns\HasStates;
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Bifrost\Database\Eloquent\Concerns\HasSchemaless;
 use Bifrost\Database\Eloquent\Concerns\HasEncryption;
-use Spatie\SchemalessAttributes\SchemalessAttributes;
 use Bifrost\Database\Eloquent\Concerns\HasAttributesResolver;
 
 class Model extends BaseModel
 {
 
-  use HasHashes, HasEncryption, HasStates, HasAttributesResolver;
+  use HasHashes, HasEncryption, HasSchemaless, HasStates, HasAttributesResolver;
 
   /**
    * Indicates when model key is UUID.
@@ -24,13 +20,6 @@ class Model extends BaseModel
    * @var bool
    */
   protected bool $uuidPrimaryKey = true;
-
-  /**
-   * Model schemaless attributes.
-   *
-   * @var array
-   */
-  protected array $schemalessAttributes = [];
 
   /**
    * Model attributes map.
@@ -116,8 +105,8 @@ class Model extends BaseModel
       return $this->decryptValue(parent::getAttributeValue($key));
     }
 
-    if (in_array($key, $this->schemalessAttributes)) {
-      return SchemalessAttributes::createForModel($this, $key);
+    if ($this->hasSchemalessBehaviour($key)) {
+      return $this->getSchemalessAttribute($key);
     }
 
     return parent::getAttributeValue($key);
@@ -145,42 +134,4 @@ class Model extends BaseModel
     return $this;
   }
 
-  /**
-   * Scope a query to apply json conditions.
-   *
-   * @param Builder $builder
-   * @param mixed ...$arguments
-   * @return Builder
-   * @throws Throwable
-   */
-  public function scopeWithJson($builder, ...$arguments): Builder
-  {
-    $schemalessAttributes = [];
-    $attributeName = Arr::first($this->schemalessAttributes) ?? 'json_data';
-
-    if (count($arguments) === 0) {
-      return $builder->whereNotNull($attributeName);
-    }
-
-    if (count($arguments) === 1) {
-      [$schemalessAttributes] = $arguments;
-      throw_if(!is_array($schemalessAttributes), new UnexpectedValueException('Array of conditions expected.'));
-    }
-
-    if (count($arguments) === 2) {
-      [$name, $value] = $arguments;
-      $schemalessAttributes = [$name => $value];
-    }
-
-    if (count($arguments) >= 3) {
-      [$attributeName, $name, $value] = $arguments;
-      $schemalessAttributes = [$name => $value];
-    }
-
-    foreach ($schemalessAttributes as $name => $value) {
-      $builder->where(str_replace('.', '->', "{$attributeName}->{$name}"), $value);
-    }
-
-    return $builder;
-  }
 }
